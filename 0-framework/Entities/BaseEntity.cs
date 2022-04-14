173d03b1d731ef0ebe8a475dbd2d6aa1b65c7623
net developer <4pouria.ahmadi.2oo1@gmail.com>
2022-04-14 14:35:42 +0430

    init main project and product category

---
+++ 0-framework/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_framework.Entities
+{
+    public class BaseEntity
+    {
+        public long Id { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+    }
+}
---
+++ SellerWebService.DataLayer/Entities/Product/ProductCategory.cs
@@ -0,0 +1,60 @@
+﻿global using _0_framework.Entities;
+global using System.ComponentModel.DataAnnotations;
+
+namespace SellerWebService.DataLayer.Entities.Product
+{
+    public class ProductCategory : BaseEntity
+    {
+        #region prop
+        [Display(Name = "نام محصول")]
+        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Name { get; set; }
+
+        [Display(Name = "عنوان سٔو")]
+        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string SeoTitle { get; set; }
+
+        [Display(Name = "توضیحات")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Description { get; set; }
+
+        [Display(Name = "توضیحات")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "ادرس عکس")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string PictureAddress { get; set; }
+
+        [Display(Name = "الت عکس")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string PictureAlt { get; set; }
+
+        [Display(Name = "عنوان عکس")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string PictureTitle { get; set; }
+
+        [Display(Name = "فعال / غیر فعال")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "توضیحات متا")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string MetaDescription { get; set; }
+
+        [Display(Name = "کیبورد")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Keywords { get; set; }
+
+        [Display(Name = "لینک خارجی")]
+        public string ExrernalLink { get; set; }
+
+        [Display(Name = "لینک داخلی")]
+        public string InternalLink { get; set; }
+        #endregion
+
+
+
+    }
+}
