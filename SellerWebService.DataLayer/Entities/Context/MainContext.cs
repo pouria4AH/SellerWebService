5d6fdb790d3eafc3f90518b48d48aba13461a6f4
net developer <4pouria.ahmadi.2oo1@gmail.com>
2022-04-16 21:05:36 +0430

    init context

---
+++ SellerWebService.DataLayer/Entities/Context/MainContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SellerWebService.DataLayer.Entities.Context
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions<MainContext> options) : base(options)
+        {
+            
+        }
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(x => x.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- SellerWebService.WebApi/Program.cs
+++ SellerWebService.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SellerWebService.DataLayer.Entities.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+#region config database
+builder.Services.AddDbContext<MainContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApiContext")));
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
