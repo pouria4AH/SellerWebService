a3fee07ff9b7389c42556333398a73f2e6979907
net developer <4pouria.ahmadi.2oo1@gmail.com>
2022-04-25 13:46:31 +0430

    ini factor in domain

--- SellerWebService.DataLayer/Entities/Factor/Factor.cs
+++ SellerWebService.DataLayer/Entities/Factor/Factor.cs
@@ -1,13 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Security.AccessControl;
+using _0_framework.Entities;
+using SellerWebService.DataLayer.Entities.Products;
 
 namespace SellerWebService.DataLayer.Entities.Factor
 {
-    public class Factor
+    public class Factor : BaseEntity
     {
+        //#region prop
+        //public long ProductId { get; set; }
+        //public Guid Code { get; set; }
+        //public string Name { get; set; }
+        //public long MainPrice { get; set; }
+        //public long FinalPrice { get; set; }
+        //public double DiscountRate { get; set; }
+        //public double Prepayment { get; set; }
+        //public long Count { get; set; }
+        //public FactorStatus FactorStatus { get; set; }
+        //#endregion
+        //#region relations
 
+        //public Product Product { get; set; }
+        //#endregion
     }
+
+    //public enum FactorStatus
+    //{
+    //    Waiting,
+    //    Accepted,
+    //    Reject,
+    //    ReadyToFinalPayed,
+    //    Ready,
+    //    Expired,
+
+    //}
 }
